fd8a8e4f659cdc9a341f07365f529fd22649408d
Przemysław Dąbek <przemek.dabek@getbase.com>
2013-06-26 01:00:52 +0200

    Initial commit
    --skip-active-record
    --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prezenter::Application.config.secret_key_base = '1025a26f69f9b845262fb2f72b5c2a225b59cf423776ecf132fc9cab47ff5d3fc7c6cff0f2617f53e724511c4659121049e950b0d166b8291bbf73deec332ccf'
