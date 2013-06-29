931d6be898b4b7fdbc03871201bd82fdf52e266f
Przemysław Dąbek <przemek.dabek@getbase.com>
2013-06-29 07:07:12 +0200

    renamed slides to presentation(s)

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,5 +1,7 @@
-class Slides
+class Presentation
   include Mongoid::Document
+  store_in collection: "slides"
+
   field :name, type: String
   field :username, type: String
   field :html, type: String
