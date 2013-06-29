0bb93ed350e0818b9662d4457ec3ee46ccc05d01
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-06-29 07:07:12 +0200

    renamed slides to presentation(s)

---
+++ app/controllers/presentations_controller.rb
@@ -0,0 +1,63 @@
+class PresentationsController < ApplicationController
+  before_action :set_presentation, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @presentations = Presentation.all
+  end
+
+  def show
+  end
+
+  def new
+    @presentation = Presentation.new
+  end
+
+  def edit
+  end
+
+  def create
+    @presentation = Presentation.new(presentation_params)
+    @presentation.username = current_user.login
+
+    respond_to do |format|
+      if @presentation.save
+        format.html { redirect_to edit_presentation_path(@presentation), notice: 'Presentation was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @presentation }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @presentation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @presentation.update(presentation_params)
+        format.html { redirect_to @presentation, notice: 'Presentation was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @presentation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @presentation.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_presentation
+      @presentation = Presentation.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def presentation_params
+      params.require(:presentation).permit(:name, :username, :html, :css, :haml, :sass)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Prezenter::Application.routes.draw do
+  root 'presentations#index'
+  resources :presentations
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -40,7 +43,7 @@ Prezenter::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
