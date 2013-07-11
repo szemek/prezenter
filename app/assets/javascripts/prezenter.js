cd8e0f36a684aa1415049446cda5ce28cb56270a
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-07-11 19:53:24 +0200

    wrapped prezenter to fire up only on pages with presentation

--- app/assets/javascripts/prezenter.js
+++ app/assets/javascripts/prezenter.js
@@ -1,101 +1,105 @@
-var index = 0;
-var sections = null;
+$(document).ready(function(){
+  $("[data-mode='viewer'], [data-mode='prezenter']").each(function(){
+    var index = 0;
+    var sections = null;
 
-var Prezenter = {
-  initialize: {},
-  notify: {},
-  update: {},
-  viewer: {
-    passive: true
-  }
-};
+    var Prezenter = {
+      initialize: {},
+      notify: {},
+      update: {},
+      viewer: {
+        passive: true
+      }
+    };
 
-// initialize
-Prezenter.initialize.sections = function() {
-  sections = document.querySelectorAll('section');
-  for (var i = 0, length = sections.length; i < length; i++) {
-    sections[i].dataset.id = i;
-  }
-};
+    // initialize
+    Prezenter.initialize.sections = function() {
+      sections = document.querySelectorAll('section');
+      for (var i = 0, length = sections.length; i < length; i++) {
+        sections[i].dataset.id = i;
+      }
+    };
 
-// notify
-Prezenter.notify.viewers = function(hash) {
-  // socket.emit('change', hash);
-}
+    // notify
+    Prezenter.notify.viewers = function(hash) {
+      // socket.emit('change', hash);
+    }
 
-// update
-Prezenter.update.hash = function() {
-  var hash = window.location.hash;
-  if(hash){
-    var position = parseInt(hash.substr(1));
-    var section = sections[position];
-    for(var i=0; i<sections.length; i++) {
-      if(sections[i] === section) {
-        section.classList.add('top');
-        index = i;
+    // update
+    Prezenter.update.hash = function() {
+      var hash = window.location.hash;
+      if(hash){
+        var position = parseInt(hash.substr(1));
+        var section = sections[position];
+        for(var i=0; i<sections.length; i++) {
+          if(sections[i] === section) {
+            section.classList.add('top');
+            index = i;
+          }
+          else {
+            sections[i].classList.remove('top');
+          }
+        }
       }
       else {
-        sections[i].classList.remove('top');
+        window.location.hash = '#0';
       }
-    }
-  }
-  else {
-    window.location.hash = '#0';
-  }
-};
+    };
 
-Prezenter.update.slide = function(event) {
-  var key = event.keyCode;
-  if(key === 37) {
-    index = Math.max(index-1, 0);
-    window.location.hash = sections[index].dataset.id;
-  }
-  else if(key === 39) {
-    index = Math.min(index+1, sections.length-1);
-    window.location.hash = sections[index].dataset.id;
-  }
-};
+    Prezenter.update.slide = function(event) {
+      var key = event.keyCode;
+      if(key === 37) {
+        index = Math.max(index-1, 0);
+        window.location.hash = sections[index].dataset.id;
+      }
+      else if(key === 39) {
+        index = Math.min(index+1, sections.length-1);
+        window.location.hash = sections[index].dataset.id;
+      }
+    };
 
-// var socket = io.connect(window.location.origin);
+    // var socket = io.connect(window.location.origin);
 
-window.addEventListener('load', function() {
-  Prezenter.initialize.sections();
-  Prezenter.update.hash();
-});
-
-window.addEventListener('hashchange', function() {
-  Prezenter.update.hash();
-});
-
-window.addEventListener('load', function() {
-  var mode = document.querySelector('#wrapper').dataset.mode;
-
-  // Prezenter mode
-  if(mode === 'prezenter') {
-    window.addEventListener('hashchange', function() {
-      Prezenter.notify.viewers(window.location.hash);
+    window.addEventListener('load', function() {
+      Prezenter.initialize.sections();
+      Prezenter.update.hash();
     });
-  }
 
-  window.addEventListener('keydown', function(event) {
-    if(mode === 'viewer' && event.keyCode === 27) {
-      Prezenter.viewer.passive = !(Prezenter.viewer.passive);
-      if(Prezenter.viewer.passive) {
-        alert("You are now in passive mode");
+    window.addEventListener('hashchange', function() {
+      Prezenter.update.hash();
+    });
+
+    window.addEventListener('load', function() {
+      var mode = document.querySelector('#wrapper').dataset.mode;
+
+      // Prezenter mode
+      if(mode === 'prezenter') {
+        window.addEventListener('hashchange', function() {
+          Prezenter.notify.viewers(window.location.hash);
+        });
       }
-      else {
-        alert("You are now in active mode");
-      }
-    }
-    if(mode === 'prezenter' || !(Prezenter.viewer.passive)) {
-      Prezenter.update.slide(event);
-    }
+
+      window.addEventListener('keydown', function(event) {
+        if(mode === 'viewer' && event.keyCode === 27) {
+          Prezenter.viewer.passive = !(Prezenter.viewer.passive);
+          if(Prezenter.viewer.passive) {
+            alert("You are now in passive mode");
+          }
+          else {
+            alert("You are now in active mode");
+          }
+        }
+        if(mode === 'prezenter' || !(Prezenter.viewer.passive)) {
+          Prezenter.update.slide(event);
+        }
+      });
+
+      // Viewer mode
+      // socket.on('update', function(hash) {
+        // if(mode === 'viewer' && Prezenter.viewer.passive) {
+          // window.location.hash = hash;
+        // }
+      // });
+    });
   });
-
-  // Viewer mode
-  // socket.on('update', function(hash) {
-    // if(mode === 'viewer' && Prezenter.viewer.passive) {
-      // window.location.hash = hash;
-    // }
-  // });
 });
