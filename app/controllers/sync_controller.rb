f410b3843cb5106cf63d8bfa7b689ef07f2644ef
Przemysław Dąbek <373530+szemek@users.noreply.github.com>
2013-07-15 20:41:28 +0200

    used live controller

--- app/controllers/sync_controller.rb
+++ app/controllers/sync_controller.rb
@@ -1,5 +1,15 @@
-class SyncController < WebsocketRails::BaseController
+class SyncController < ApplicationController
+  include ActionController::Live
+
   def change
-    broadcast_message :update, message
+    $cache.write('hash', params[:hash])
+    render nothing: true
+  end
+
+  def update
+    hash = $cache.read('hash')
+    response.headers['Content-Type'] = 'text/event-stream'
+    response.stream.write "data: #{hash}\n\n"
+    response.stream.close
   end
 end
