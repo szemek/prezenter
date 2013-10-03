79a7f7d53176b7503370164d68a71c7c63fdedd4
Przemysław Dąbek <przemek.dabek@getbase.com>
2013-10-03 07:14:26 +0200

    simplified controller (removed unnecessary respond_to blocks

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,9 +1,13 @@
 class PresentationsController < ApplicationController
+  respond_to :html
+
   before_action :set_presentation, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, only: [:create, :edit, :update, :destroy]
 
   def index
     @presentations = Presentation.all
+
+    respond_with(@presentations)
   end
 
   def edit
@@ -13,35 +17,24 @@ class PresentationsController < ApplicationController
     @presentation = Presentation.new(presentation_params)
     @presentation.username = current_user.login
 
-    respond_to do |format|
-      if @presentation.save
-        format.html { redirect_to edit_presentation_path(@presentation), notice: 'Presentation was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @presentation }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @presentation.errors, status: :unprocessable_entity }
-      end
+    if @presentation.save
+      redirect_to edit_presentation_path(@presentation), notice: 'Presentation was successfully created.'
+    else
+      render action: 'new'
     end
   end
 
   def update
-    respond_to do |format|
-      if @presentation.update_attributes(presentation_params)
-        format.html { redirect_to edit_presentation_path(@presentation), notice: 'Presentation was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @presentation.errors, status: :unprocessable_entity }
-      end
+    if @presentation.update_attributes(presentation_params)
+      redirect_to edit_presentation_path(@presentation), notice: 'Presentation was successfully updated.'
+    else
+      render action: 'edit'
     end
   end
 
   def destroy
     @presentation.destroy
-    respond_to do |format|
-      format.html { redirect_to root_path }
-      format.json { head :no_content }
-    end
+    redirect_to root_path
   end
 
   private
